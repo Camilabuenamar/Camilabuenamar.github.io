eab70964e237e18cd3eae6aa48a3f239b3bae98e
camilabuenamar <camila.buenamar@gmail.com>
2019-09-26 13:47:00 +0100

    info added to about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,14 +11,28 @@ class AboutMe extends React.Component {
             <p className="title is-1 has-text-centered" id="aboutme">About Me</p>
             <div className="columns">
               <div className="column is-three-fifths">
-                <p className="subtitle is-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                <p className="content text">I am a business graduate who started a women’s empowerment organization in LATAM (Latin America) and this allowed me to further develop my skills in thought leadership. This experience immersed me in women’s representation in technology and the technology itself. I have inspired young women to study within the STEM fields and have challenged myself to study software engineering in the UK. This allowed me to understand the technologies used in tech companies from the operations to the business. Today, my dream is to grow a tech company with my business and tech knowledge and to continue encouraging more women to pursue a career in tech.</p>
                 <div className="education-info">
-                  <p className="title is-6" id="no-bottom-margin">Education:</p>
-                  <div className="content text" id="education">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                  <div className="title is-6" id="no-bottom-margin">Education:</div>
+                  <div className="subtitle is-6" id="education">
+                  ・Summer 2019 <span className="has-text-weight-semibold">Software Engineering Immersive</span> General Assembly, London, United Kingdom. <br />
+                  ・2013 - 2018 <span className="has-text-weight-semibold">Bachelor of Business Administration</span> Universidad de los Andes, Bogota, Colombia.<br />
+                  ・Fall 2017 <span className="has-text-weight-semibold">University Innovation Fellows Program</span> Hasso Plattner Institute of Design at Stanford University, California, USA.
+
+                  </div>
                 </div>
                 <div className="experience-info">
-                  <p className="title is-6" id="no-bottom-margin">Experience:</p>
-                  <div className="content text" id="experience">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                  <div className="title is-6" id="no-bottom-margin">Experience:</div>
+                  <div className="subtitle is-6" id="experience">
+                  ・August 2018 <span className="has-text-weight-semibold">Speaker and Facilitator</span> Loop: Innovation week of Montevideo, Montevideo, Uruguay.<br />
+                  ・Summer 2018 <span className="has-text-weight-semibold">Start-up scouting Intern</span> Rockstart Accelerator, Bogota, Colombia.<br />
+                  ・2016-2018 <span className="has-text-weight-semibold">Co-founder and President</span> Uniandes Women in Business, Bogota, Colombia.<br />
+                  ・2016, 2017 <span className="has-text-weight-semibold">Harvard Undergraduate Women in Business Campus Ambassador Leader</span> Harvard University, Boston, Massachusetts, USA.<br />
+                  ・Summer 2017 <span className="has-text-weight-semibold">Women in Technology International Programs Coordinator</span> Foundation Women for Colombia, Bogota, Colombia.<br />
+                  ・2016-2017 <span className="has-text-weight-semibold">Teacher Assistant</span> Universidad de los Andes, Bogotá, Colombia.<br />
+                  ・2015-2016 <span className="has-text-weight-semibold">Sub manager</span> Buenaventura Import Customs Broker, Bogota, Colombia.<br />
+                  ・2013-2014 <span className="has-text-weight-semibold">Human Resources Coordinator</span> Buenaventura Import Customs Broker, Bogota, Colombia.<br />
+                  </div>
                 </div>
               </div>
               <div className="column image-column has-text-centered is-two-fifths">
